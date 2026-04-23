e24bd31ddeed5b665014e8e45a4c5cc7bcbc2ed3
smahfuz <mahfuz.duetcse@gamil.com>
2026-04-23 16:30:31 +0600

    committe page and a new member added

--- frontend/src/app/pages/committees/committees.ts
+++ frontend/src/app/pages/committees/committees.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
 
+interface CommitteeMember {
+  name: string;
+  role: string;
+  imageUrl: string;
+}
+
 @Component({
   selector: 'app-committees',
   imports: [],
   templateUrl: './committees.html',
   styleUrl: './committees.scss',
 })
-export class Committees {}
+export class Committees {
+  members: CommitteeMember[] = [
+    {
+      name: 'Mahfuz Ahammed',
+      role: 'Member',
+      imageUrl: '/images/committees/450529863_3778228039119886_3234417432257102295_n.jpg'
+    }
+  ];
+}
