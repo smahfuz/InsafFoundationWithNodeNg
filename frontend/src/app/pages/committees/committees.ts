4d4bb0aab143ae533c7711f10f4f2046787c80f7
smahfuz <mahfuz.duetcse@gamil.com>
2026-04-24 16:08:38 +0600

    commitee hide

--- frontend/src/app/pages/committees/committees.ts
+++ frontend/src/app/pages/committees/committees.ts
@@ -24,20 +24,20 @@ export class Committees {
       role: 'Member',
       imageUrl: '/images/committees/151031898_696085994415772_4853668275872735270_n.jpg'
     },
-    {
-      name: 'Mahfuz Ahammed',
-      role: 'Member',
-      imageUrl: '/images/committees/450529863_3778228039119886_3234417432257102295_n.jpg'
-    },
-    {
-      name: 'Akib',
-      role: 'Member',
-      imageUrl: '/images/committees/488886821_2499612927037111_4723461957892870766_n.jpg'
-    },
-    {
-      name: 'Rosin Hossen',
-      role: 'Member',
-      imageUrl: '/images/committees/660282092_1490183569298229_4755263152437248916_n.jpg'
-    }
+    // {
+    //   name: 'Mahfuz Ahammed',
+    //   role: 'Member',
+    //   imageUrl: '/images/committees/450529863_3778228039119886_3234417432257102295_n.jpg'
+    // },
+    // {
+    //   name: 'Akib',
+    //   role: 'Member',
+    //   imageUrl: '/images/committees/488886821_2499612927037111_4723461957892870766_n.jpg'
+    // },
+    // {
+    //   name: 'Rosin Hossen',
+    //   role: 'Member',
+    //   imageUrl: '/images/committees/660282092_1490183569298229_4755263152437248916_n.jpg'
+    // }
   ];
 }
