237b928247860a284e30dbbb44e1f1903af6726d
smahfuz <mahfuz.duetcse@gamil.com>
2026-04-24 09:32:31 +0600

    header menue update

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common'; // 👈 এটা add করো
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule], // 👈 এখানে add
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   title = 'Insaf Foundation';
+  isOpen = false;
 }
